d289f75d13b47de18036bc55777d18585f6d5f16
Madeline Beke <mattbeke@gmail.com>
2024-11-30 11:24:43 -0500

    styling changes

--- Assets/script.js
+++ Assets/script.js
@@ -61,7 +61,7 @@ function generatePassword(){
 }
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector(".generate");
 
 // Write password to the #password input
 function writePassword() {
